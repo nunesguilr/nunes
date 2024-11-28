77da93cce37bd73dae3f80b2d3393207107d375f
resident2x <guermenunes2@gmail.com>
2024-11-28 20:54:13 -0300

    terminando basic kotlin

---
+++ README.md
@@ -0,0 +1,1 @@
+# Estudando Kotlin por curso
--- src/main/kotlin/text.kt
+++ src/main/kotlin/text.kt
@@ -1,5 +1,13 @@
-fun main(){
-    println("Hello World")
+fun main() {
+
+    // Formas de definir variaveis
+    // definição de tipo explicita
+
+    val estrada: String = "Meia nove"
+
+    //Definição implicita
+
+    val caminhao = "Vermelhão"
 
     // Testando variaveis
     val i: Int = 2
@@ -15,19 +23,65 @@ val n3: Double = numberByte
 */
 
 // funcionalidade to permite mudar o tipo do valor da variavel
-    val n1 : Int = numberByte.toInt()
+    val n1: Int = numberByte.toInt()
     println(n1)
 
-    val n2 : String = numberByte.toString()
+    val n2: String = numberByte.toString()
     println(n2)
 
-    val n3 : Float = numberByte.toFloat()
+    val n3: Float = numberByte.toFloat()
     println(n3)
 
 // entendendo variaveis Var é mutavel e Val imutavel
     var nome = "luis"
-    nome  = "leonardo"
+    nome = "leonardo"
+    val carro = "corola"
+//   Erro de sintaxe carro = "fusca"
 
+// Interpolação com variaveis
 
+    var number = 2
+    var number2 = 3
 
+    println("Hoje é dia $number e amanhã é dia $number2")
+    println("Daqui três dias é dia ${number2 + number}")
+
+// Teste com condições
+
+    if (number > number2) {
+        println("Numero 1 é maior que numero 2")
+    } else {
+        println("Numero 2 é maior que numero 1")
+    }
+    if (number2 in 1..10) {
+        println("Numero 2 estar no intervalo de 1 a 10")
+    }
+    when (number2) {
+        0 -> println("numero igual a zero")
+        in 1..10 -> ("Numero no intervalo de 1 a 10")
+        else -> println("Numero não alcançou nenhuma dessa condições")
+    }
+
+// Testando nulos (estudar mais depois)
+
+    var etd = null
+
+    var test: Int? = etd
+
+    if (number == null) {
+        number = number.dec()
+    }
+
+// estudando vetores e loops
+// lista imutavel
+    var listTop = listOf("Luiz", "Vaca", "Pedra")
+    println(listTop)
+
+    val myList = mutableListOf("Luiz", "Vaca", "Pedra")
+    myList.remove("Pedra")
+
+    val school = arrayOf("shark", "salmon", "minnow")
+    println(java.util.Arrays.toString(school))
+
+    // Existem diferentes tipagens para os arrays e listas
 }
